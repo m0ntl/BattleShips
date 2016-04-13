27e242ce899959c5d410316c8d33fd575e5fb0f1
montl <montlake.a@gmail.com>
2016-04-14 00:53:43 +0300

    Implemented Coordinate class as parcelable

--- app/src/main/java/com/bs/battleships/Coordinate.java
+++ app/src/main/java/com/bs/battleships/Coordinate.java
@@ -1,9 +1,12 @@
 package com.bs.battleships;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by montl on 10/04/2016.
  */
-public class Coordinate {
+public class Coordinate implements Parcelable{
     private int x,y;
     public Coordinate(int x, int y){
         this.x = x;
@@ -22,4 +25,33 @@ public class Coordinate {
     public void setY(int y){
         this.y = y;
     }
+
+    protected Coordinate(Parcel in) {
+        x = in.readInt();
+        y = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(x);
+        dest.writeInt(y);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Coordinate> CREATOR = new Parcelable.Creator<Coordinate>() {
+        @Override
+        public Coordinate createFromParcel(Parcel in) {
+            return new Coordinate(in);
+        }
+
+        @Override
+        public Coordinate[] newArray(int size) {
+            return new Coordinate[size];
+        }
+    };
 }
